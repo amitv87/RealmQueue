0935a99145721f4fbcc4957eb82289aa0140313e
amit verma <amitv87@gmail.com>
2018-04-05 20:13:49 +0530

    ignore build directories

--- buildDeps.sh
+++ buildDeps.sh
@@ -1,5 +1,5 @@
 set -e
-set -x
+# set -x
 
 NUM_CPU=$(getconf _NPROCESSORS_ONLN)
 
@@ -11,8 +11,8 @@ sh autogen.sh
 make -j$NUM_CPU
 
 cd $ROOT_DIR/deps/realm-object-store
-mkdir -p out
-cd out
+mkdir -p .build
+cd .build
 cmake -DCMAKE_BUILD_TYPE=Release -DREALM_CORE_PREFIX=$ROOT_DIR/deps/realm-core -DREALM_PLATFORM=$(uname) ..
 make -j$NUM_CPU
 
